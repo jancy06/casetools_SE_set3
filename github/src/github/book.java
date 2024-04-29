70dd5e7c903b5fb77c3030deaca4e61af7eac811
kim fernandes <kimsfdf@gmail.com>
2024-04-29 10:49:53 +0530

    commit

---
+++ github/src/github/book.java
@@ -0,0 +1,22 @@
+package github;
+
+public class book {
+	private String title;
+	private String Author;
+	public book(String title,String Author) {
+		this.title=title;
+		this.Author=Author;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		
+	}
+
+}
